3ed74af893a2159563638ceab09327776e98ca25
JB <jasper.bartscher@gmail.com>
2020-08-10 15:19:39 +0200

    restframework und prototyp model für wort und description

--- Bildungsspre_chApplikation/models.py
+++ Bildungsspre_chApplikation/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class Word(models.Model):
+    name = models.CharField(max_length=255)
+    creation_date = models.DateField()
+
+
+class Field(models.Model):
+    word = models.ForeignKey(Word, on_delete=models.CASCADE)
+    description = models.TextField()
