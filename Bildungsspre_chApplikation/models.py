197546b24a93a7d7fa56d0709dc96e2f73707422
JB <jasper.bartscher@gmail.com>
2020-08-10 15:49:22 +0200

    überarbeitetes prototyp model für wort, feld und description

--- Bildungsspre_chApplikation/models.py
+++ Bildungsspre_chApplikation/models.py
@@ -2,10 +2,27 @@ from django.db import models
 
 
 class Word(models.Model):
-    name = models.CharField(max_length=255)
-    creation_date = models.DateField()
+    word = models.CharField(max_length=255)
+    creation_date = models.DateTimeField(auto_now_add=True)
+    related = models.ManyToManyField("self")  # related words
+
+    class Meta:
+        ordering = ['word']
 
 
 class Field(models.Model):
+    title = models.CharField(max_length=255)
+    creation_date = models.DateTimeField(auto_now_add=True)
+    related = models.ManyToManyField("self")  # related fields
+
+    class Meta:
+        ordering = ['title']
+
+class Description(models.Model):
+    field = models.ForeignKey(Field, default="Wissenschaftlich", on_delete=models.SET_DEFAULT)
     word = models.ForeignKey(Word, on_delete=models.CASCADE)
     description = models.TextField()
+    creation_date = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        order_with_respect_to = 'word'
